af1a17927c8f9eaddd97a0f28039e4e68b457501
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-05-20 19:42:11 -0500

    add Message and input componenets, remove default content from vue cli

---
+++ vue-webpack/src/Input.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input type="text" v-model="message">
+    <p>{{message}}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      message: ''
+    }
+  }
+}
+</script>
