53d060bb195b1e97c37d3e5935601df528618ddf
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-09-10 18:33:19 -0500

    add gitignore in top level, add server folder for components practice

---
+++ vue-course/Section6/servers/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <page-header></page-header>
+        <hr>
+        <div class="row">
+            <servers></servers>
+        </div>
+        <hr>
+        <page-footer></page-footer>
+    </div>
+</template>
+
+<script>
+import Footer from "./components/shared/Footer.vue";
+import Header from "./components/shared/Header.vue";
+import Servers from "./components/server/Servers.vue";
+export default {
+    components: {
+        'page-footer': Footer,
+        'page-header': Header,
+        'servers': Servers,
+    }
+}
+</script>
+
+<style>
+
+</style>
