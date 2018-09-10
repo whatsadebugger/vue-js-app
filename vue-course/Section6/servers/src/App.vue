078778085469d80449f71dddc5af728b683be0a0
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-09-10 18:39:04 -0500

    es6 style key value pairs

--- vue-course/Section6/servers/src/App.vue
+++ vue-course/Section6/servers/src/App.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="container">
-        <page-header></page-header>
+        <appHeader></appHeader>
         <hr>
         <div class="row">
             <servers></servers>
         </div>
         <hr>
-        <page-footer></page-footer>
+        <appFooter></appFooter>
     </div>
 </template>
 
 <script>
-import Footer from "./components/shared/Footer.vue";
-import Header from "./components/shared/Header.vue";
+import appFooter from "./components/shared/Footer.vue";
+import appHeader from "./components/shared/Header.vue";
 import Servers from "./components/server/Servers.vue";
 export default {
     components: {
-        'page-footer': Footer,
-        'page-header': Header,
-        'servers': Servers,
+        appFooter,
+        appHeader,
+        Servers,
     }
 }
 </script>
