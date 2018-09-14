bb24b973b6cf59128543bd3b307a1c0014adc221
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-09-14 18:50:05 -0500

    passing data from child to parent and emitting an event

--- vue-course/Section7/src/components/UserDetail.vue
+++ vue-course/Section7/src/components/UserDetail.vue
@@ -3,6 +3,7 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
@@ -17,6 +18,10 @@ export default {
     methods: {
         switchName() {
             return this.myName.split("").reverse().join("");
+        },
+        resetName() {
+            this.myName = 'resetname';
+            this.$emit('nameWasReset', this.myName);
         }
     }
 }
