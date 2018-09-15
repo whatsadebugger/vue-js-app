139ad316b898fc240fc8b07e5e526524ce710896
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-09-14 19:10:26 -0500

    emitting events from child components to update the props

--- vue-course/Section7/src/components/User.vue
+++ vue-course/Section7/src/components/User.vue
@@ -7,10 +7,12 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail>
+                <app-user-detail :myName="name" @nameWasReset="name = $event" :resetFn="resetName" :userAge="age"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit :userAge="age"
+                                @ageWasEdited="age = $event"
+                ></app-user-edit>
             </div>
         </div>
     </div>
@@ -23,12 +25,16 @@
     export default {
         data: function() {
             return {
-                name: 'Ahmad'
+                name: 'Ahmad',
+                age: 27
             };
         },
         methods: {
             changeName() {
                 this.name = 'emaNehTdegnahC';
+            },
+            resetName() {
+                this.name = 'Max';
             }
         },
         components: {
--- vue-course/Section7/src/components/UserDetail.vue
+++ vue-course/Section7/src/components/UserDetail.vue
@@ -3,7 +3,9 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ switchName() }}</p>
+        <p>User Age: {{ userAge }}</p>
         <button @click="resetName">Reset Name</button>
+        <button @click="resetFn()">Reset Name</button>
     </div>
 </template>
 
@@ -13,14 +15,16 @@ export default {
         myName: {
             type: String,
             default: 'Bobby'
-        }
+        },
+        resetFn: Function,
+        userAge: Number
     },
     methods: {
         switchName() {
             return this.myName.split("").reverse().join("");
         },
         resetName() {
-            this.myName = 'resetname';
+            this.myName = 'Max';
             this.$emit('nameWasReset', this.myName);
         }
     }
