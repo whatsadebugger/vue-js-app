e447ede2075b8da1c5b6984d6de34a96f32066ce
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-09-22 20:04:42 -0500

    Add ability to Fix servers

--- vue-course/Section7Exercise/src/components/Server/ServerDetails.vue
+++ vue-course/Section7Exercise/src/components/Server/ServerDetails.vue
@@ -1,11 +1,24 @@
 <template>
     <div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
+        <p v-if="!server">Please select a Server</p>
+        <p v-else>Server #{{ server.id }} Server Status: {{ server.status }}</p>
     </div>
-
 </template>
 
 <script>
+import { serverBus } from  '../../main'
+export default {
+    date: function() {
+        return {
+            server: null
+        }
+    },
+    created() {
+        serverBus.$on('serverSelected', (server) => {
+            this.server = server;
+        })
+    }
+}
 </script>
 
 <style>
--- vue-course/Section7Exercise/src/components/Server/Servers.vue
+++ vue-course/Section7Exercise/src/components/Server/Servers.vue
@@ -1,16 +1,28 @@
 <template>
-    <div class="col-xs-12 col-sm-6">
-        <ul class="list-group">
-            <li
-                    class="list-group-item"
-                    v-for="index in 5">
-                Server #{{ index }}
-            </li>
-        </ul>
-    </div>
+<div class="col-xs-12 col-sm-6">
+    <ul class="list-group">
+        <app-server v-for="server in servers" :server="server"></app-server>
+    </ul>
+</div>
 </template>
 
 <script>
+    import Server from './Server.vue';
+    export default {
+        data: function() {
+            return {
+                servers: [
+                    {id: 1, status: 'Normal'},
+                    {id: 2, status: 'Down'},
+                    {id: 3, status: 'Degraded'},
+                    {id: 4, status: 'Normal'}
+                ]
+            }
+        },
+        components: {
+            appServer: Server
+        }
+    }
 </script>
 
 <style>
