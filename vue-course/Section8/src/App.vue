5ca28fe45ff67b94ee58d7d3c131f7f861a651d2
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-09-23 15:40:14 -0500

    Start adding quote component

--- vue-course/Section8/src/App.vue
+++ vue-course/Section8/src/App.vue
@@ -1,19 +1,28 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-               
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <h2>The Quote</h2>
+          <p>A wonder Quote</p>
+        </app-quote>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import Quote from "./components/Quote.vue";
 
-    export default {
-        
-    }
+export default {
+  components: {
+    appQuote: Quote
+  }
+};
 </script>
 
-<style>
+<style scoped>
+h2 {
+  color: red;
+}
 </style>
