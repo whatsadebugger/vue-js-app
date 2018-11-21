525aeb8f700e64988fc99a4f3e70750850279ebf
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-11-20 18:08:26 -0600

    add section10, make a quotes app

---
+++ vue-course/Section10/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <new-quote @quoteAdded="newQuote"></new-quote>
+        <quote-grid :quotes="quotes" @quoteDelete="deleteQuote"></quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info: Click on a quote to delete it
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import AppHeader from "./components/Header.vue";
+
+export default {
+  data: function() {
+    return {
+      quotes: ["I'm here for a good time not a long time"],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete some quotes you have reached the maximum");
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  },
+  components: {
+    quoteGrid: QuoteGrid,
+    newQuote: NewQuote,
+    appHeader: AppHeader
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vue-course/Section10/src/components/QuoteGrid.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row">
+      <app-quote v-for="(quote, i) in quotes" @click.native="deleteQuote(index)">{{quote}}</app-quote>
+    </div>
+</template>
+
+<script>
+import Quote from './Quote.vue';
+export default {
+  props: ["quotes"],
+  components: {
+    appQuote: Quote
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit('quoteDelete', index)
+    }
+  }
+};
+</script>
+
+<style></style>
