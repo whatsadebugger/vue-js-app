a09f1ea5d3e4c3bd6602c98029941c2dfe466d41
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-12-02 17:01:21 -0600

    start section14 animations

---
+++ vue-course/Section14/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr>
+        <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+        <hr>
+        <div class="alert alert-info" v-if="show">This is some info</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  }
+};
+</script>
+
+<style>
+</style>
