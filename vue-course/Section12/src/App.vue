f61e78736c3d2380111bd77afe070d6cff5ba64e
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-12-02 15:32:06 -0600

    creating a custom directive locally and globally

--- vue-course/Section12/src/App.vue
+++ vue-course/Section12/src/App.vue
@@ -1,19 +1,63 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives</h1>
-
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Built-in Directives</h1>
+        <p v-text="'Some Text'"></p>
+        <p v-html="'<strong> String tag </strong>'"></p>
+      </div>
     </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-highlight="'firebrick'">Color this</p>
+        <p v-local-highlight.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Color this</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
+export default {
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        let delay = 0;
+        if (binding.modifiers["delayed"]) {
+          delay = 3000;
+        }
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor
+                ? (currentColor = mainColor)
+                : (currentColor = secondColor);
+
+              if (binding.arg == "background") {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, binding.value.delay);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if (binding.arg == "background") {
+              el.style.backgroundColor = binding.value.mainColor;
+            } else {
+              el.style.color = binding.value.mainColor;
+            }
+          }, binding.value.delay);
+        }
+      }
     }
+  }
+};
 </script>
 
 <style>
-
 </style>
--- vue-course/Section12/src/main.js
+++ vue-course/Section12/src/main.js
@@ -1,6 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Custom directives can be created and have access to five hooks
+// you can pass arguments to directives in vue using directive:arg=""
+// we can also add custom modifiers which are passed in as an object with the binding
+// parameter
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    // el.style.backgroundColor = 'green';
+    // el.style.backgroundColor = binding.value;
+    let delay = 0;
+    if (binding.modifiers['delayed']) {
+      delay = 3000;
+    }
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
