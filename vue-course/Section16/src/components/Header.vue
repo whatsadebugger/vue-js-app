fece7b3ae22065fb20486fde0e03ae47b6c58534
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-12-05 08:04:10 -0600

    add new routes, add header with new routes

---
+++ vue-course/Section16/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <ul class="nav nav-pills">
+    <li role="presentation">
+      <a><router-link to="/">Home</router-link></a>
+    </li>
+    <li role="presentation">
+      <a><router-link to="/user">User</router-link></a>
+    </li>
+  </ul>
+</template>
--- vue-course/Section16/src/components/Home.vue
+++ vue-course/Section16/src/components/Home.vue
@@ -8,3 +8,6 @@
 
     </div>
 </template>
+
+<script>
+</script>
