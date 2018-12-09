c7b90d892dc208d71e479cd165970f1d322262c9
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-12-09 13:53:48 -0600

    add header and stock templates

--- vue-course/StockTrader/src/components/stocks/stocks.vue
+++ vue-course/StockTrader/src/components/stocks/stocks.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Stocks stock component</h1>
+  <div>
+    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+  </div>
 </template>
+
+<script>
+import stock from './stock.vue'
+export default {
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: 'BMW', price: 100},
+        { id: 2, name: 'Google', price: 200},
+        { id: 3, name: 'Apple', price: 250},
+        { id: 4, name: 'Twitter', price: 8},
+      ]
+    }
+  },
+  components: {
+    appStock: stock
+  }
+}
+</script>
+
