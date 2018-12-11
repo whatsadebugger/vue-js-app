2b186837ff4e2f5064a1323c82e3b317efd82bed
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-12-10 18:16:21 -0600

    random auth

--- vue-course/axios/src/axios-auth.js
+++ vue-course/axios/src/axios-auth.js
@@ -4,6 +4,6 @@ const instance = axios.create({
   baseURL: 'https://vueclass-8171b.firebaseio.com'
 })
 
-instance.defaults.headers['Authorization'] = 'token'
+instance.defaults.headers['RandomAuth'] = 'token'
 
 export default instance;
